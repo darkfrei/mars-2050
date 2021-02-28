9ced70bea31f2e6256ccd15d3cddabba6ed90f92
darkfrei <34188677+darkfrei@users.noreply.github.com>
2021-02-28 17:43:45 +0100

    Update data.lua

--- data.lua
+++ data.lua
@@ -1,8 +1,14 @@
-space={}
+space={} -- space has Earth, Sun and all bodies
+space.sun={}
+soace.sun.buddy_names = 
+{"mercury", "venus", "earth", "mars", "ceres", "pallas", 
+  "jupiter", 
+  "saturn", "uranus", "neptune"}
 space.planetes={}
 earth={}
 -- table.insert(space, earth)
 earth.name="earth"
+earth.system="sun"
 earth.buildings={}
 earth.rockets={}
 earth.orbit={}
